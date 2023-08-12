78ea5bd0f1d969763214152f29efa973f6ab5543
Ben Hanna <benpaulhanna@gmail.com>
2023-08-12 11:20:37 -0400

    Implemented grid draw.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 #define WIN_W 640
 #define WIN_H 480
+#define PATTERN_SIZE 8
 
 typedef struct 
 {
@@ -15,14 +16,55 @@ SDL_Renderer *renderer;
 SDL_Event event;
 
 int running = 1;
+int pattern[PATTERN_SIZE] = {
+    0x00,
+    0x00,
+    0x04,
+    0x06,
+    0xff,
+    0x06,
+    0x04,
+    0x00
+};
 
-Color bg_color = { .r = 0, .g = 0, .b = 0 };
-Color fg_color = { .r = 0xff, .g = 0xff, .b = 0xff };
+Color bg_color = { .r = 0xff, .g = 0xff, .b = 0xff };
+Color fg_color = { .r = 0xb2, .g = 0xb2, .b = 0xb2 };
 
 void draw()
 {
-    SDL_SetRenderDrawColor(renderer, bg_color.r, bg_color.g, bg_color.b, 0xff);
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
     SDL_RenderClear(renderer);
+
+    SDL_Rect rect;
+    rect.w = 0x20;
+    rect.h = 0x20;
+
+    for (int i = 0; i < PATTERN_SIZE; i++)
+    {
+        rect.x = (PATTERN_SIZE - 1) * rect.w;
+        rect.y = i * rect.h;
+
+        for (int j = 0; j < PATTERN_SIZE; j++)
+        {
+            int k = (pattern[i] >> j) & 1;
+            int e = pattern[i] >> j;
+            printf("%d, %d, %d\n", rect.x, rect.y, e);
+
+            if (k)
+            {
+                SDL_SetRenderDrawColor(renderer, fg_color.r, fg_color.g, fg_color.b, 0xff);
+            }
+            else 
+            {
+                SDL_SetRenderDrawColor(renderer, bg_color.r, bg_color.g, bg_color.b, 0xff);
+            }
+
+            SDL_RenderFillRect(renderer, &rect);
+
+            rect.x -= 0x20;
+        }
+    }
+
     SDL_RenderPresent(renderer);
 }
 
@@ -51,6 +93,8 @@ int main()
     while (running != 0)
     {
         draw();
+
+        SDL_Delay(2000);
 
         while (SDL_PollEvent(&event))
         {
